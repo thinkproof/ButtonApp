7d6f258a3686ede936371db1b115d01c0e4d95ce
thinkproof <evan.ling02@gmail.com>
2020-02-07 15:30:55 -0500

    i

--- app/src/main/java/com/lingevan/buttonapp/MainActivity.java
+++ app/src/main/java/com/lingevan/buttonapp/MainActivity.java
@@ -6,21 +6,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
+    TextView displayText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         submitButton = findViewById(R.id.clickButton);
+        displayText=findViewById(R.id.textLabel);
         submitButton.setOnClickListener(new View.OnClickListener() {
+            int i =0;
             @Override
             public void onClick(View view) {
-                System.out.println("Hello mom!");
-                Log.i("testButton", "Hi dad!");
+                Log.i("testButton", "Amazing message!");
+                displayText.setText(""+i++);
             }
         });
+    }
+
+    public void smile(View view) {
+        //different way to make
     }
 }
